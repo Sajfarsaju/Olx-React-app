cb02c5847dc5ea885fd20bef29085aa7312aa1e0
Sajfarsaju <sajfarmmuhammad@gmail.com>
2023-09-13 12:54:39 +0530

    5th commit olx-post feature

---
+++ src/Components/View/View.js
@@ -0,0 +1,52 @@
+import React,{useEffect , useState , useContext} from 'react';
+import { firebaseContext } from '../../store/firebaseContext';
+import { PostContext } from '../../store/PostContext';
+
+import './View.css';
+function View() {
+
+  const [userDetails, setUserDetails] = useState()
+
+  const {postDetails} = useContext(PostContext)
+
+  const {firebase} = useContext(firebaseContext)
+
+  useEffect(()=>{
+    const {userId} = postDetails
+    firebase.firestore().collection('users').where('id' , '==' , userId).get().then((res)=>{
+      res.forEach(doc =>{
+        console.log(doc.data(),'========');
+        setUserDetails(doc.data())
+      })
+    })
+  },[])
+  
+
+  return (
+    <div className="viewParentDiv">
+      <div className="imageShowDiv">
+        <img
+          src={postDetails.url}
+          alt=""
+        />
+      </div>
+      <div className="rightSection">
+        <div className="productDetails">
+          <p>&#x20B9; {postDetails.price} </p>
+          <span>{postDetails.name}</span>
+          <p>{postDetails.category}</p>
+          <span>{postDetails.createdAt}</span>
+        </div>
+
+    { userDetails && 
+        <div className="contactDetails">
+          <p>Seller details</p>
+          <p>{userDetails.userName}</p>
+          <p>{userDetails.phone}</p>
+        </div>
+    }
+      </div>
+    </div>
+  );
+}
+export default View;
